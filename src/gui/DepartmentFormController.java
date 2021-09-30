42607ea48a6a49283b7de35c9dd21609149a5dec
josemar666 <josemarlob1@hotmail.com>
2021-09-30 20:58:01 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.util.Alerts;
@@ -17,6 +19,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entites.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartamentService;
 
 public class DepartmentFormController implements Initializable{
@@ -60,7 +63,10 @@ public class DepartmentFormController implements Initializable{
 		service.saveOrUpdate(entity);
 		notifyDataChangeListeners();
 		Utils.currentStage(event).close();
-		}catch(DbException e) {
+		}catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch(DbException e) {
 			Alerts.showAlert("Error saving object",null ,  e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -71,9 +77,16 @@ public class DepartmentFormController implements Initializable{
 		
 	}
 	private Department getFormData() {
+		ValidationException exception = new ValidationException("Validation Error !!!");
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			 exception.addError("Name", "Field can't be empty");
+		}
 		Department obj = new Department();
 		obj.setId(Utils.tryParseToInt( txtId.getText()));
 		obj.setName(txtName.getText());
+		if( exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		return obj;
 	}
 	@FXML
@@ -101,6 +114,12 @@ public class DepartmentFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		labelErrorName.setText(errors.get("Name"));
+		
 	}
 
 }
